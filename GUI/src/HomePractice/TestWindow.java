7eaa055329af7e512a33edeeb6fa1f2b0a154e26
levan <levan@HoaThao>
2022-06-06 00:57:34 -0600

    test

---
+++ GUI/src/HomePractice/TestWindow.java
@@ -0,0 +1,22 @@
+package HomePractice;
+
+/**
+* Class description: This is to create Window
+*
+* @author Hoa Le (855432)
+*
+*/
+
+public class TestWindow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Create a window
+		PracticeMyWindow myFrame = new PracticeMyWindow ();
+		// set appear in the screen
+		myFrame.setVisible(true);
+	}
+
+}
